a573f3d9a6a9b5abc21bfb43fa53631db92c6cf3
Adekunle Ayodele <adekunleayodele@gmail.com>
2023-10-04 15:54:15 +0100

    React JS Checkpoint Commit

---
+++ src/Components/Body.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Body = () => {
+  return (
+    <div>
+      <div className="mb-3">
+          <label for="exampleFormControlInput1" className="form-label">Name</label>
+          <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+        </div>
+        <div className="mb-3">
+          <label for="exampleFormControlInput1" className="form-label">Email address</label>
+          <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+        </div>
+        <div className="mb-3">
+          <label for="exampleFormControlInput1" className="form-label">Phone</label>
+          <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+        </div>
+        <div className="mb-3">
+          <label for="exampleFormControlInput1" className="form-label">Password</label>
+          <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+        </div>
+        
+    </div>
+  )
+}
+
+export default Body
---
+++ src/Components/Form.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div>
+      <div className="mb-3">
+          <label for="exampleFormControlTextarea1" className="form-label">Message</label>
+          <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+    </div>
+  )
+}
+
+export default Form
